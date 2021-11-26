6cf1908b4dfb88fcf28745d1a1342dcbd8c45f80
Steven Ngov <gl.steven.ngov@gmail.com>
2021-11-26 22:31:16 +0000

    Add letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,44 @@
+const eqArrays = function(array1, array2) {
+  let equalOrNot = true;
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        equalOrNot = false;
+        break;
+      }
+    }
+  } else {
+    equalOrNot = false;
+  }
+  return equalOrNot;
+};
+
+
+const assertArraysEqual = function(array1, array2) {
+  let arraysEqual = eqArrays(array1, array2);
+  if (arraysEqual === true) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+
+const letterPositions = function(sentence) {
+
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions("lighthouse in the house"));
+assertArraysEqual(letterPositions("hello").e, [1]);
